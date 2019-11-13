28f25a57a3c168b03c116a0fc806942873b34741
Michael Stoffer <mstoffer@michaelstoffer.com>
2019-11-12 21:38:44 -0500

    Initial Commit

---
+++ Random Users/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Random Users
+//
+//  Created by Michael Stoffer on 11/12/19.
+//  Copyright © 2019 Michael Stoffer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ Random Users/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Random Users
+//
+//  Created by Michael Stoffer on 11/12/19.
+//  Copyright © 2019 Michael Stoffer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
